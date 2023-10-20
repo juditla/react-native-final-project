d095d609ae031b474fe0e41cf215b6e0ec2834d4
Judit Langer <judit.langer@aon.at>
2023-10-20 21:28:59 +0200

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,10 @@
+export type Studio = {
+  id: number;
+  name: string;
+  address: string;
+  city: string;
+  postalCode: number;
+  ownerId: number;
+  longitude?: string;
+  latitude?: string;
+};
