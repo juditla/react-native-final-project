6bd93ae6c35156bec81c3d015dc04cffb36fad18
Judit Langer <judit.langer@aon.at>
2023-11-06 11:12:27 +0100

    Add user and tattooImage type

--- types.ts
+++ types.ts
@@ -11,8 +11,27 @@ export type Studio = {
 
 export type Artist = {
   id: number;
+  userId: number;
   name: string;
   style: string;
   description: string;
   studioId?: number;
+  tattooImages?: [TattooImage];
+};
+
+export type User = {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  roleId: number;
+  createDate: string;
+};
+
+export type TattooImage = {
+  id: number;
+  name: string;
+  picture: string;
+  artistId: number;
+  style: string;
 };
