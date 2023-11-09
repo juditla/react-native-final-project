4a29e39796fb3deeb2bb1b5f8441260841386737
Judit Langer <judit.langer@aon.at>
2023-11-09 16:27:39 +0100

    Delete unnecessary files

--- app/(tabs)/messages.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function Index() {
-  return (
-    <View>
-      <Text>This is your messages page</Text>
-    </View>
-  );
-}
--- app/messages/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function index() {
-  return (
-    <View>
-      <Text>This is your messages page</Text>
-    </View>
-  );
-}
