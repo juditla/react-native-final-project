761f627df455cf3066c13f2a5f5b88ba4de53c74
Judit Langer <judit.langer@aon.at>
2023-11-17 10:16:14 +0100

    Add custom colors to chat bubbles

--- app/(tabs)/messages/[conversationId].tsx
+++ app/(tabs)/messages/[conversationId].tsx
@@ -1,25 +1,20 @@
-import { Stack, useLocalSearchParams, usePathname } from 'expo-router';
+import { Stack, useFocusEffect, useLocalSearchParams } from 'expo-router';
 import React, { useCallback, useContext, useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
-import { Button, TextInput } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 import { User } from '../../../types';
+import modifyMessage from '../../../util/modifyMessage';
+import sortArrayByDate from '../../../util/sortArrayByDate';
 import UserContext from '../../UserProvider';
 import { apiDomain } from '../studios';
 
-type Message = {
+export type Message = {
   id: number;
   senderId: number;
   coinversationtId: number;
   createDate: string;
   text: string;
   sender: User;
-};
-
-type MessageForGiftedChat = Message & {
-  createdAt: string;
-  _id: number;
-  user: User;
 };
 
 type GiftedChatMessage = [
@@ -41,32 +36,17 @@ const styles = StyleSheet.create({
 });
 
 function modifyAndSortMessagesArray(array: Message[]): MessageForGiftedChat[] {
-  const modifiedArray = array.map((message) => {
+  const modifiedMessageArray = array.map((message) => {
+    console.log(message);
     return modifyMessage(message);
   });
-  const sortedArray = modifiedArray.sort(
-    (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
-  );
-  return sortedArray;
-}
-
-function modifyMessage(message: Message): MessageForGiftedChat {
-  const modifiedMessage = message;
-  modifiedMessage.createdAt = message.createDate;
-  console.log('datenbankdate', message.createDate);
-  modifiedMessage._id = message.id;
-  modifiedMessage.user = {
-    _id: message.senderId,
-    name: message.sender.firstName,
-    avatar: '',
-  };
-  return modifiedMessage;
+  console.log(modifiedMessageArray);
+  return sortArrayByDate(modifiedMessageArray);
 }
 
 export default function SingleConversation() {
   const { conversationId, conversationPartner } = useLocalSearchParams();
   const [messages, setMessages] = useState<MessageForGiftedChat[] | []>([]);
-  const [newMessage, setNewMessage] = useState('');
   const userContext = useContext(UserContext);
 
   async function sendMessageHandler(newMessageArray: GiftedChatMessage) {
@@ -86,36 +66,43 @@ export default function SingleConversation() {
     }
   }
 
+  const getMessagesByConversationId = async () => {
+    try {
+      const response = await fetch(`${apiDomain}/messages/${conversationId}`);
+      const json = await response.json();
+      const modifiedMessagesforGiftedChat = modifyAndSortMessagesArray(json);
+      setMessages(modifiedMessagesforGiftedChat);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   useEffect(() => {
-    const getMessagesByConversationId = async () => {
-      try {
-        const response = await fetch(`${apiDomain}/messages/${conversationId}`);
-        const json = await response.json();
-        const modifiedMessagesforGiftedChat = modifyAndSortMessagesArray(json);
-        setMessages(modifiedMessagesforGiftedChat);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
     getMessagesByConversationId()
       .then(() => console.log('there', messages))
       .catch((error) => error);
   }, [conversationId]);
 
   const onSend = useCallback(async (message = []) => {
-    console.log('giftedchatdate', message[0].createdAt);
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, message),
     );
     await sendMessageHandler(message);
   }, []);
 
+  useFocusEffect(
+    useCallback(() => {
+      getMessagesByConversationId()
+        .then()
+        .catch((error) => error);
+    }, []),
+  );
+
   return (
     <>
       <Stack.Screen
         options={{
           title: conversationPartner ? conversationPartner : '',
+          headerTintColor: 'black',
           //   headerShown: false,
         }}
       />
@@ -127,34 +114,30 @@ export default function SingleConversation() {
         user={{
           _id: userContext?.currentUser?.id,
         }}
+        renderBubble={(props) => {
+          return (
+            <Bubble
+              {...props}
+              textStyle={{
+                right: {
+                  color: 'white',
+                },
+                left: {
+                  color: 'white',
+                },
+              }}
+              wrapperStyle={{
+                left: {
+                  backgroundColor: '#05C19C',
+                },
+                right: {
+                  backgroundColor: '#474554',
+                },
+              }}
+            />
+          );
+        }}
       />
     </>
-
-    // <>
-    //   <View>
-    //     <Text>This is your conversation</Text>
-    //     {messages.map((message) => {
-    //       message.createdAt = message.createDate;
-    //       message._id = message.id;
-    //       message.user = {
-    //         _id: message.sender.id,
-    //         name: message.sender.firstName,
-    //         avatar: '',
-    //       };
-    //       return (
-    //         <Text key={`id-${message.id}`}>
-    //           {message.user.name}:{message.text}
-    //         </Text>
-    //       );
-    //     })}
-    //   </View>
-    //   <View>
-    //     <TextInput
-    //       value={newMessage}
-    //       onChangeText={(val) => setNewMessage(val)}
-    //     />
-    //     <Button onPress={async () => await sendMessageHandler()}>Send</Button>
-    //   </View>
-    // </>
   );
 }
