8c58f6bbea37ec317bbec43f69aa24bce56f7189
Judit Langer <judit.langer@aon.at>
2023-11-21 16:28:59 +0100

    Comment out unused code

--- app/(tabs)/artists/FilterComponent.tsx
+++ app/(tabs)/artists/FilterComponent.tsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import { Button, Modal, PaperProvider, Portal, Text } from 'react-native-paper';
+// import React, { useState } from 'react';
+// import { Button, Modal, PaperProvider, Portal, Text } from 'react-native-paper';
 
-const containerStyle = {
-  backgroundColor: 'white',
-  padding: 20,
-  height: 300,
-  position: 'absolute',
-  top: 0,
-  width: 300,
-  // margin: 20,
-  // zIndex: 3,
-};
+// const containerStyle = {
+//   backgroundColor: 'white',
+//   padding: 20,
+//   height: 300,
+//   position: 'absolute',
+//   top: 0,
+//   width: 300,
+//   // margin: 20,
+//   // zIndex: 3,
+// };
 
-export default function FilterComponent() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+// export default function FilterComponent() {
+//   const [isModalVisible, setIsModalVisible] = useState(false);
 
-  return (
-    <PaperProvider>
-      <Portal>
-        <Modal visible={isModalVisible} contentContainerStyle={containerStyle}>
-          <Text>Example Modal.</Text>
-          <Button mode="outlined" onPress={() => setIsModalVisible(false)}>
-            Close
-          </Button>
-        </Modal>
-      </Portal>
-      <Button onPress={() => setIsModalVisible(true)}>Show</Button>
-    </PaperProvider>
-  );
-}
+//   return (
+//     <PaperProvider>
+//       <Portal>
+//         <Modal visible={isModalVisible} contentContainerStyle={containerStyle}>
+//           <Text>Example Modal.</Text>
+//           <Button mode="outlined" onPress={() => setIsModalVisible(false)}>
+//             Close
+//           </Button>
+//         </Modal>
+//       </Portal>
+//       <Button onPress={() => setIsModalVisible(true)}>Show</Button>
+//     </PaperProvider>
+//   );
+// }
