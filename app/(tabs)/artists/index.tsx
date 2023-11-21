81df4c4e1550abd49de9236a83ad138f37b85532
Judit Langer <judit.langer@aon.at>
2023-11-21 16:27:47 +0100

    Try different font

--- app/(tabs)/artists/index.tsx
+++ app/(tabs)/artists/index.tsx
@@ -1,3 +1,24 @@
+import {
+  MontserratAlternates_100Thin,
+  MontserratAlternates_100Thin_Italic,
+  MontserratAlternates_200ExtraLight,
+  MontserratAlternates_200ExtraLight_Italic,
+  MontserratAlternates_300Light,
+  MontserratAlternates_300Light_Italic,
+  MontserratAlternates_400Regular,
+  MontserratAlternates_400Regular_Italic,
+  MontserratAlternates_500Medium,
+  MontserratAlternates_500Medium_Italic,
+  MontserratAlternates_600SemiBold,
+  MontserratAlternates_600SemiBold_Italic,
+  MontserratAlternates_700Bold,
+  MontserratAlternates_700Bold_Italic,
+  MontserratAlternates_800ExtraBold,
+  MontserratAlternates_800ExtraBold_Italic,
+  MontserratAlternates_900Black,
+  MontserratAlternates_900Black_Italic,
+  useFonts,
+} from '@expo-google-fonts/montserrat-alternates';
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { router, Stack } from 'expo-router';
 import { useContext, useEffect, useState } from 'react';
@@ -7,7 +28,6 @@ import { Artist } from '../../../types';
 import UserContext from '../../UserProvider';
 import { apiDomain } from '../studios';
 import ArtistItem from './ArtistItem';
-import FilterComponent from './FilterComponent';
 
 const renderItem = (item: { item: Artist }) => (
   <ArtistItem artist={item.item} />
@@ -15,8 +35,6 @@ const renderItem = (item: { item: Artist }) => (
 
 const styles = StyleSheet.create({
   container: {
-    // marginLeft: 10,
-    // marginRight: 10,
     marginTop: 10,
   },
   rowContainer: {
@@ -33,6 +51,26 @@ export default function Index() {
   const [artists, setArtists] = useState<Artist[]>([]);
   const userContext = useContext(UserContext);
   const bottomTabHeight = useBottomTabBarHeight();
+  let [fontsLoaded] = useFonts({
+    MontserratAlternates_100Thin,
+    MontserratAlternates_100Thin_Italic,
+    MontserratAlternates_200ExtraLight,
+    MontserratAlternates_200ExtraLight_Italic,
+    MontserratAlternates_300Light,
+    MontserratAlternates_300Light_Italic,
+    MontserratAlternates_400Regular,
+    MontserratAlternates_400Regular_Italic,
+    MontserratAlternates_500Medium,
+    MontserratAlternates_500Medium_Italic,
+    MontserratAlternates_600SemiBold,
+    MontserratAlternates_600SemiBold_Italic,
+    MontserratAlternates_700Bold,
+    MontserratAlternates_700Bold_Italic,
+    MontserratAlternates_800ExtraBold,
+    MontserratAlternates_800ExtraBold_Italic,
+    MontserratAlternates_900Black,
+    MontserratAlternates_900Black_Italic,
+  });
 
   useEffect(() => {
     const getArtists = async () => {
@@ -52,25 +90,32 @@ export default function Index() {
       .then()
       .catch((error) => error);
   }, [userContext]);
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Stack.Screen
-        options={{
-          headerShown: false,
-        }}
-      />
-      <View style={styles.rowContainer}>
-        <Text variant="headlineMedium">Artists</Text>
-        <Icon source="filter-variant" size={25} />
-        {/* <FilterComponent /> */}
-      </View>
-      <FlatList
-        data={artists}
-        renderItem={renderItem}
-        keyExtractor={(artist: Artist) => artist.id.toString()}
-        contentContainerStyle={{ paddingBottom: bottomTabHeight }}
-      />
-    </SafeAreaView>
-  );
+  if (!fontsLoaded) {
+    return undefined;
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Stack.Screen
+          options={{
+            headerShown: false,
+          }}
+        />
+        <View style={styles.rowContainer}>
+          <Text
+            variant="headlineMedium"
+            style={{ fontFamily: 'MontserratAlternates_400Regular' }}
+          >
+            Artists
+          </Text>
+          <Icon source="filter-variant" size={25} />
+        </View>
+        <FlatList
+          data={artists}
+          renderItem={renderItem}
+          keyExtractor={(artist: Artist) => artist.id.toString()}
+          contentContainerStyle={{ paddingBottom: bottomTabHeight }}
+        />
+      </SafeAreaView>
+    );
+  }
 }
