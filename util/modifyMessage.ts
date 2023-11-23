f989d50f54ec43fc2a9893d03daed379e700114c
Judit Langer <judit.langer@aon.at>
2023-11-23 20:54:21 +0100

    Fix typescript date bug

--- util/modifyMessage.ts
+++ util/modifyMessage.ts
@@ -1,21 +1,16 @@
 import { IMessage } from 'react-native-gifted-chat';
 import { Message } from '../app/(tabs)/messages/[conversationId]';
-import { User } from '../types';
-
-// type MessageForGiftedChat = Message & {
-//   createdAt: string;
-//   _id: number;
-//   user: User;
-// };
 
 export default function modifyMessage(message: Message): IMessage {
-  const modifiedMessage = message;
-  modifiedMessage.createdAt = message.createDate;
-  modifiedMessage._id = message.id;
-  modifiedMessage.user = {
-    _id: message.senderId,
-    name: message.sender.firstName,
-    avatar: '',
+  const modifiedMessage = {
+    ...message,
+    createdAt: new Date(message.createDate),
+    _id: message.id,
+    user: {
+      _id: message.senderId,
+      name: message.sender.firstName,
+      avatar: '',
+    },
   };
   return modifiedMessage;
 }
