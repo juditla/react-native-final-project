33520206f5e1ea040f9fa039b0ad6b5d00ab52b5
Judit Langer <judit.langer@aon.at>
2024-01-08 20:24:18 +0100

    Add IMessage to imports in util function

--- util/sortArrayByDate.ts
+++ util/sortArrayByDate.ts
@@ -1,4 +1,6 @@
-export default function sortArrayByDate(unsortedArray) {
+import { IMessage } from 'react-native-gifted-chat';
+
+export default function sortArrayByDate(unsortedArray: IMessage[]) {
   const sortedArray = unsortedArray.sort(
     (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
   );
